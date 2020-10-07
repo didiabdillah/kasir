bdc15f8df5ddf34fa43f6b0e8b706881839d2674
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-10-07 20:27:58 +0700

    Mencoba file css yang telah disiapkan

--- app/controllers/Transaksi.php
+++ app/controllers/Transaksi.php
@@ -5,5 +5,10 @@ class Transaksi extends Controller
 
     public function index()
     {
+        $data['judul'] = "Transaksi";
+
+        $this->view('Templates/header', $data);
+        $this->view('Transaksi/index');
+        $this->view('Templates/footer');
     }
 }
---
+++ app/views/Templates/footer.php
@@ -0,0 +1,9 @@
+<script src="<?= BASEURL; ?>js/jquery-3.1.1.min.js"></script>
+
+<script src="<?= BASEURL; ?>js/popper.min.js"></script>
+
+<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+
+</body>
+
+</html>
