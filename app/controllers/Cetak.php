350948a01f0235b0ab9d351ec10f41e80f2522ad
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-10-08 08:04:10 +0700

    Cetak => Membuat Controller Cetak dan instansiasi mPDF di method print()

---
+++ app/controllers/Cetak.php
@@ -0,0 +1,23 @@
+<?php
+
+class Cetak extends Controller
+{
+
+    public function index()
+    {
+        $data['judul'] = "Barang";
+        $data['barang'] = $this->model('Barang_model')->getAllBarang();
+
+        $this->view('Templates/header', $data);
+        $this->view('Cetak/index', $data);
+        $this->view('Templates/footer');
+    }
+
+    public function print()
+    {
+        require_once 'vendor/autoload.php';
+
+        // Create an instance of the class:
+        $mpdf = new \Mpdf\Mpdf();
+    }
+}
